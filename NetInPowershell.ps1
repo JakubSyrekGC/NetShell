f03c8a3afe7af76618ff866529a8de630d5103bd
JakubSyrekGC <71333889+JakubSyrekGC@users.noreply.github.com>
2020-10-06 10:13:42 +0200

    Add files via upload

---
+++ NetInPowershell.ps1
@@ -0,0 +1,51 @@
+﻿$code = @"
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace NETShell
+{    
+    public class Runner
+        {
+            public static async Task<int> ReturnNumberAsync()
+            {
+                int result = await Task.Run(async () =>
+                {
+                    await Task.Delay(1000);
+                    return 42;
+                });
+                return result;
+            }
+        
+            public static async Task<int> RunJobsAsync()
+            {
+                var t = await ReturnNumberAsync();
+                return t;
+            }
+         }
+	public class NetTasks
+	{
+		public static void Main()  
+        {
+            Runner.RunJobsAsync().Wait();
+        }
+	}
+}
+"@
+ 
+Add-Type -TypeDefinition $code -Language CSharp	
+function Await-Task {
+    param (
+        [Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+        $task
+    )
+
+    process {
+        while (-not $task.AsyncWaitHandle.WaitOne(200)) { }
+        $task.GetAwaiter().GetResult()
+    }
+}
+
+$r = [NETShell.Runner]::RunJobsAsync() | Await-Task
+$r 
+
+#iex "[NETShell.NetTasks]::Main()"
